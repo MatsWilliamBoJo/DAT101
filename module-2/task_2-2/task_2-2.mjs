05dd68436db628d7bc1b8f4e0b072b974e82ddf1
Mats William Boseth Johansen <matsbjohansen@gmail.com>
2025-09-18 13:39:25 +0200

    Ferdig med alle programeringsoppgavene i 2.2

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -39,33 +39,56 @@ printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-const exchangeRate = 8.6 / 76 // USD to NOK
-const money = exchangeRate * 100;
-printOut("100 USD is equal to " + money.toFixed(2) + " NOK.");
+const dollarToNOK = 76 / 8.6
+const TotalDollar = 54
+const TotalNOK = TotalDollar * dollarToNOK
+printOut("54 dollars is equal to " + TotalNOK.toFixed(0) + " NOK.");
+printOut
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let sentence = "There is much between heaven and earth that we do not understand.";
+const countCharacters = sentence.length;
+printOut("The sentence: \"" + sentence + "\" has " + countCharacters + " characters.");
+printOut("The character at position 19 is " + sentence.charAt(19) + ".");
+printOut("The characters between position 35 and 8 characters forward is " + sentence.substr(35, 8) + ".");
+printOut("The index at which the word 'earth' starts is " + sentence.indexOf("earth") + ".");
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("5 is grater than 3: " + (5 > 3));
+printOut("Is 7 grater than or equal to 7: " + (7 >= 7));
+printOut("Is a grater then b: " + ("a" > "b"));
+printOut("Is 1 less than a: " + (1 < "a"));
+printOut("arne is not equal to thomas: " + ("arne" != "thomas"));
+printOut("2 is equal to 5: " + (2 == 5));
+printOut("abcd is grater than bcd: " + ("abcd" > "bcd"));
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const num1 = Number("254");
+const num2 = Number("57.23");
+const num3 = Number("25 kroner");
+
+printOut('"254" converted to a number is: ' + num1);
+printOut('"57.23" converted to a number is: ' + num2);
+printOut('"25 kroner" converted to a number is: ' + num3);
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const r = Math.floor(Math.random() * 360) + 1;
+printOut("Random number r (1 <= r <= 360): " + r);
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const totalDays = 131;
+const weeks = Math.floor(totalDays / 7);
+const days = totalDays % 7;
+printOut(totalDays + " days is " + weeks + " weeks and " + days + " days.");
 printOut(newLine);
