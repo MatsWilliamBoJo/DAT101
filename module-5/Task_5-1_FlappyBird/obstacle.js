2c47f260d1a87dece5ff9ff50c43f32f29799540
Mats William Boseth Johansen <matsbjohansen@gmail.com>
2026-01-19 08:27:27 +0100

    jobber med flappybird 2

--- module-5/Task_5-1_FlappyBird/obstacle.js
+++ module-5/Task_5-1_FlappyBird/obstacle.js
@@ -1,16 +1,38 @@
 "use strict";
 import { TSprite } from "libSprite";
 
-export class TObstacle {
-    #spUp;
-    #spDown;
-    constructor(aSpcvs, aSPI){
-        const x = 580;
-        this.#spDown = new TSprite(aSpcvs , aSPI, x, 300 );
-        this.#spDown.index = 4;
-        this.#spUp = new TSprite(aSpcvs , aSPI, x, -125 );
-        this.#spUp.index = 1;
+const EasyFlyerGap = 150; //Gap between the up and down obstacle
+const HardFlyerGap = 100;
+const MinimumProtrusion = 30;
+
+export class TObstacle{
+  #spUp;
+  #spDown;
+  #spi;
+  constructor(aSpcvs, aSPI){
+    const x = 600;
+    this.#spi = aSPI;
+    // Generate random gap height, based on difficulty settings
+    const gap = Math.ceil(Math.random() * (EasyFlyerGap - HardFlyerGap) + HardFlyerGap);
+    const minTop = -this.#spi.height + MinimumProtrusion; // Minimum top position for upper obstacle
+    const maxTop = -MinimumProtrusion; // Maximum top position for upper obstacle
+    // Genrate random top position for upper obstacle
+    let top = Math.ceil(Math.random() * (maxTop - minTop) + minTop);
+    const minBottom = 400 - MinimumProtrusion; // Minimum bottom position for lower obstacle
+    let topWithGap = this.#spi.height + top + gap; // Initial position of bottom obstacle based on the height of the sprite, gap, and top 
+    if(topWithGap > minBottom){
+      // The top with gap is too low, adjust top and keep the gap constant
+      const adjustment = topWithGap - minBottom;
+      top -= adjustment;
+      topWithGap = this.#spi.height + top + gap; // Recalculate topWithGap after adjustment
     }
+
+    this.#spDown = new TSprite(aSpcvs, aSPI, x, topWithGap);
+    this.#spDown.index = 2;
+    this.#spUp = new TSprite(aSpcvs, aSPI, x, top);
+    this.#spUp.index = 3;
+  }
+
 
     //Properties
     get x(){
